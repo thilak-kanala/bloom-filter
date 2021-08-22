2b9c5dbf6155cf82c5dda4a8cfc8532bdd81a053
thilak-reddy <thilakkanala@gmail.com>
2021-08-22 12:18:05 +0530

    Revert "modified data_preprocessing.py to generate fake query words"
    This reverts commit 663f7804e4b2ea7ca8b0f3e0acef5e6d8cf474ca.

--- data_preprocessing.py
+++ data_preprocessing.py
@@ -1,9 +1,5 @@
 import os
 import sys
-import random
-
-random_words = [
-'green', 'double', 'disability', 'fold', 'bet', 'tendency', 'extract', 'deputy', 'trolley', 'indoor', 'van', 'rain', 'preference', 'laboratory', 'realize', 'wine', 'pressure', 'hilarious', 'outlook', 'voter', 'account', 'irony', 'end', 'toll', 'am', 'creed', 'absorb', 'allocation', 'hurt', 'last', 'key', 'medieval', 'oil', 'stir', 'hair', 'breathe', 'favorable', 'solve', 'rich', 'quest', 'trap', 'drawer', 'animal', 'fitness', 'construct', 'factory', 'humanity', 'sugar', 'concrete', 'hallway']
 
 print('== Data Preprocessing Started ... ==')
 
@@ -46,22 +42,13 @@ modified_words_query_fh.write(" ")
 
 
 i = 0
-for word in words[:(N_WORDS/2)]:
+for word in words[:N_WORDS]:
     modified_words_query_fh.write(word)
     modified_words_query_fh.write(" ")
 
-    progress = ((i / (N_WORDS/2)) * 100)
+    progress = ((i / N_WORDS) * 100)
     if (progress % 20 == 0):
-        print(f'real query words: {progress} %')
-    i = i + 1
-
-for i in range(0, (N_WORDS/2)):
-    modified_words_query_fh.write(random.choice(random_words))
-    modified_words_query_fh.write(" ")
-
-    progress = ((i / (N_WORDS/2)) * 100)
-    if (progress % 20 == 0):
-        print(f'fake query words: {progress} %')
+        print(f'{progress} %')
     i = i + 1
 
 modified_words_query_fh.close()
